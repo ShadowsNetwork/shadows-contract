3bf1beafa1f87e07447f0d311d0a6578c8c985bc
lyiyun1024 <lyiyun1024@gmail.com>
2021-03-22 14:42:05 +0800

    add testUtils

--- test/contracts/Shadows.js
+++ test/contracts/Shadows.js
@@ -1,26 +1,50 @@
-const { expect } = require("chai");
+require('.'); 
 const Oracle = artifacts.require("Oracle");
 const Shadows = artifacts.require("Shadows");
 const FeePool = artifacts.require("FeePool");
 const SafeDecimalMath = artifacts.require("SafeDecimalMath");
 const AddressResolver = artifacts.require("AddressResolver");
-const { toBytes32 } = require("../utils");
+const Synth = artifacts.require("Synth");
+const { toBytes32, toUnit } = require("../testUtils");
 
 contract("Shadows", async (accounts) => {
-  let shadows, oracle, feePool, safeDecimalMath, addressResolver;
+  let shadows, oracle, feePool, addressResolver, safeDecimalMath;
+
+  const [deployerAccount, owner, oracleAccount, account2, account3] = accounts;
+
+  const [xUSD, xAUD, xEUR, DOWS, xBTC, xETH] = [
+    "xUSD",
+    "xAUD",
+    "xEUR",
+    "DOWS",
+    "xBTC",
+    "xETH",
+  ].map(toBytes32);
+
+  before(async () => {
+    safeDecimalMath = await SafeDecimalMath.new();
+    await Shadows.link(safeDecimalMath);
+    await Oracle.link(safeDecimalMath);
+    await FeePool.link(safeDecimalMath);
+  });
 
   beforeEach(async () => {
     addressResolver = await AddressResolver.new();
-    safeDecimalMath = await SafeDecimalMath.new();
 
-    await Shadows.link(safeDecimalMath);
     shadows = await Shadows.new();
-    shadows.initialize(addressResolver.address);
+    shadows.initialize(addressResolver.address, { from: owner });
 
-    await Oracle.link(safeDecimalMath);
+    //oracle
     oracle = await Oracle.new();
+    oracle.initialize(
+      oracleAccount,
+      [xAUD, xEUR, DOWS, xBTC],
+      ["0.5", "1.25", "0.1", "5000"].map(toUnit),
+      {
+        from: oracleAccount,
+      }
+    );
 
-    await FeePool.link(safeDecimalMath);
     feePool = await FeePool.new();
 
     await addressResolver.importAddresses(
@@ -29,8 +53,43 @@ contract("Shadows", async (accounts) => {
     );
   });
 
-  it("should set params on initialize", async () => {
-    assert.equal(await shadows.name(), "Shadows Network Token");
-    assert.equal(await shadows.symbol(), "DOWS");
+  describe("constructor", () => {
+    it("should set params on initialize", async () => {
+      assert.equal(await shadows.name(), "Shadows Network Token");
+      assert.equal(await shadows.symbol(), "DOWS");
+      assert.equal(await shadows.owner(), owner);
+      assert.equal(await shadows.resolver(), addressResolver.address);
+    });
+  });
+
+  describe("adding and removing synths", () => {
+    it("should allow adding a Synth contract", async () => {
+      const previousSynthCount = await shadows.availableSynthCount();
+      const synth = await Synth.new();
+      await synth.initialize(
+        "Synth TEST1",
+        "xTEST",
+        toBytes32("xTEST"),
+        addressResolver.address,
+        { from: owner }
+      );
+
+      assert.bnEqual(await synth.owner(), owner);
+
+      await shadows.addSynth(synth.address, { from: owner });
+
+      // Assert that we've successfully added a Synth
+      assert.bnEqual(
+        await shadows.availableSynthCount(),
+        previousSynthCount.add(web3.utils.toBN(1))
+      );
+      // Assert that it's at the end of the array
+      assert.equal(
+        await shadows.availableSynths(previousSynthCount),
+        synth.address
+      );
+      // Assert that it's retrievable by its currencyKey
+      assert.equal(await shadows.synths(toBytes32("xTEST")), synth.address);
+    });
   });
 });
--- test/utils.js
+++ test/utils.js
@@ -1,10 +1,15 @@
-'use strict';
-
-const w3utils = require('web3-utils');
+"use strict";
+const BN = require('bn.js');
+const w3utils = require("web3-utils");
+const { toBN, asciiToHex, toWei } = require("web3-utils");
 
 /**
  * Converts a string into a hex representation of bytes32, with right padding
  */
-const toBytes32 = key => w3utils.rightPad(w3utils.asciiToHex(key), 64);
+const toBytes32 = (key) => w3utils.rightPad(asciiToHex(key), 64);
 
-module.exports = { toBytes32 };
+const toUnit = (amount) => toBN(toWei(amount.toString(), "ether"));
+
+const UNIT = toWei(new BN('1'), 'ether')
+
+module.exports = { toBytes32, toUnit, toWei };
