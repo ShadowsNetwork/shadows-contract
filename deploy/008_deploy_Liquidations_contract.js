0babc46737f109ea60c4938d72e6a29d688f31a0
enjoynodejser <enjoynodejs@gmail.com>
2021-07-20 10:03:37 +0800

    deploy Liquidations

---
+++ deploy/008_deploy_Liquidations_contract.js
@@ -0,0 +1,19 @@
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy, get } = deployments;
+  const { deployer, shadowsOwner } = await getNamedAccounts();
+
+  const safeDecimalMath = await get("SafeDecimalMath");
+  const addressResolver = await get("AddressResolver");
+
+  await deploy("Liquidations", {
+    from: deployer,
+    proxy: {
+      methodName: 'initialize',
+      proxyContract: 'OptimizedTransparentProxy',
+    },
+    args: [addressResolver.address],
+    log: true,
+    libraries: { SafeDecimalMath: safeDecimalMath.address },
+  });
+};
+module.exports.tags = ['Liquidations']
