4ac6b6bf7b1ce5eb8f9176509e803ef60e90ada2
lyiyun1024 <lyiyun1024@gmail.com>
2022-02-07 10:29:20 +0800

    updateRates

--- scripts/updateRates.js
+++ scripts/updateRates.js
@@ -1,23 +1,26 @@
-const { toBN } = require('web3-utils');
-const { toBytes32, bytesToString, fromUnit, toUnit, currentTime } = require("../utils");
+const { toBN } = require("web3-utils");
+const {
+  toBytes32,
+  bytesToString,
+  fromUnit,
+  toUnit,
+  currentTime,
+} = require("../utils");
 
 (async () => {
   const { deploy, get, execute, read } = deployments;
   const { deployer, shadowsOwner } = await getNamedAccounts();
 
-  console.log(toUnit("0.118935").toString());
-  const [xUSD, DOWS, xBTC, xETH] = [
-    "ShaUSD",
-    "DOWS",
-    "xBTC",
-    "xETH",
-  ].map(toBytes32);
+  console.log(toUnit("0.068197").toString());
+  const [xUSD, DOWS, xBTC, xETH] = ["ShaUSD", "DOWS", "xBTC", "xETH"].map(
+    toBytes32
+  );
   await execute(
-    'Oracle',
+    "Oracle",
     { from: deployer },
-    'updateRates',
+    "updateRates",
     [DOWS],
-    [toUnit("0.118935").toString()],
-    await currentTime(),
+    [toUnit("0.068197").toString()],
+    await currentTime()
   );
 })();
